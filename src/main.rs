b5105b5a5400ce1320eb9bb07a81d785573b96fd
Ron <ronsurf23@gmail.com>
2020-07-19 17:33:01 -0700

    master

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ use structopt::StructOpt;
 struct Options {
     #[structopt(default_value = "Meowz!")]
     /// What does the cat say?
-    message: String // [1]
+    message: String, // [1]
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 
 fn main() {
-    let options = Options::from_args(); // [2]
+    let options = Options::from_args(); //
     let message = options.message;
+
+    let eye = if options.dead { "x" } else { "o" }; // [1]
+
     println!("{}", message);
-    // ...print the cat
+    println!(" \\");
+    println!("  \\");
+    println!("     /\\_/\\");
+    println!("    ( {eye} {eye} )", eye=eye); // [2]
+    println!("    =( I )=");
 }
